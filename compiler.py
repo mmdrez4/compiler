94acb4ec762857ca6a3c963188ab73cde8ac8454
mmdrez4 <mohammadreza.dn80@gmail.com>
2021-10-19 21:14:49 +0330

    update first two states

--- compiler.py
+++ compiler.py
@@ -43,74 +43,87 @@ while True:
         pointer += 1
         continue
 
-    if current_state == State.START:
-        pass
-    elif current_state == State.NUM:
+    # TODO if our example is 125d what should we do?
+    if current_state == State.NUM:
         if re.match(num_regex, character):
-            pass
+            lexeme += character
         else:
             lexeme += character
+            if re.match(r'[0-9]+', character):
+                # error handling for 125d
+                errors_file.write("(" + lexeme + " Invalid number) ")
+                lexeme = ''
+                current_state = State.START
+    # TODO if our example is elseff what should we do?
     elif current_state == State.ID or current_state == State.KEYWORD:
         if re.match(ID_regex, character):
-            pass
-        elif re.match(keyword_regex, character):
-            pass
-        else:
             lexeme += character
+        else:
+            current_state = State.START
+            if re.match(keyword_regex, lexeme):
+                tokens_file.write("(KEYWORD, " + lexeme + ") ")
+            else:
+                tokens_file.write("(ID, " + lexeme + ") ")
+            lexeme = ''
+            can_read = False
     elif current_state == State.SYMBOL:
-        if re.match(symbol_regex, character):
-            pass
+        if character == '=':
+            tokens_file.write("(SYMBOL, " + "==" + ") ")
+
         else:
-            lexeme += character
+            tokens_file.write("(SYMBOL, " + "=" + ") ")
     elif current_state == State.COMMENT:
         if re.match(comment_regex, character):
             pass
         else:
             lexeme += character
-    elif current_state == State.WHITESPACE:
-        if re.match(space_regex, character):
-            pass
-        else:
-            lexeme += character
-
-    if character == "*":
-        if len(lexeme) > 1:
-            pass
+    # does'nt be in any state!
     else:
-        if re.match(space_regex, character):
-            pass
+        can_read = True
+        if character == "*":
+            if len(lexeme) > 1:
+                pass
         else:
-            lexeme += character
+            if re.match(space_regex, character):
+                pass
+            else:
+                lexeme += character
 
-    if re.match(num_regex, i):
-        tokens_file.write("(NUM, " + i + ") ")
+        if re.match(num_regex, i):
+            tokens_file.write("(NUM, " + i + ") ")
 
-    elif re.match(ID_regex, i):
-        tokens_file.write("(ID, " + i + ") ")
-        if i not in symbols_file.read():
-            symbols_file.write(i)
+        elif re.match(ID_regex, i):
+            tokens_file.write("(ID, " + i + ") ")
+            if i not in symbols_file.read():
+                symbols_file.write(i)
 
-    elif re.match(keyword_regex, i):
-        tokens_file.write("(KEYWORD, " + i + ") ")
+        elif re.match(keyword_regex, i):
+            tokens_file.write("(KEYWORD, " + i + ") ")
 
-    elif re.match(symbol_regex, i):
-        tokens_file.write("(SYMBOL, " + i + ") ")
+        elif re.match(symbol_regex, character):
+            if character == '=':
+                current_state = State.SYMBOL
+            else:
+                tokens_file.write("(SYMBOL, " + character + ") ")
 
-    elif re.match(comment_regex, i):
-        continue
+        elif re.match(comment_regex, i):
+            continue
+        else:
+            errors_file.write("(" + character + ", Invalid input) ")
+
+        # elif re.match(r'[0-9]+'):
+        #     errors_file.write("(" + i + " Invalid number) ")
+        # elif i.__eq__("*/"):
+        #     errors_file.write("(*/, Unmatched comment) ")
+        # elif i.startswith("/*"):
+        #     errors_file.write("(/*, Unclosed comment) ")
+        # else:
+        #     errors_file.write("(" + i + ", Invalid input) ")
 
     # elif re.match(ID_regex, i):
     #     tokens_file.write("(KEYWORD, " + i + ") ")
     #     pointer += 1
 
-    elif re.match(r'[0-9]+'):
-        errors_file.write("(" + i + " Invalid number) ")
-    elif i.__eq__("*/"):
-        errors_file.write("(*/, Unmatched comment) ")
-    elif i.startswith("/*"):
-        errors_file.write("(/*, Unclosed comment) ")
-    else:
-        errors_file.write("(" + i + ", Invalid input) ")
 
 pointer += 1
 errors_file.write('\n')
