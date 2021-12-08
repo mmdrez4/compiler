5b69589155cd143e40937c86206b83e842f62cdf
mmdrez4 <mohammadreza.dn80@gmail.com>
2021-12-08 15:31:20 +0330

    add code

--- parser.py
+++ parser.py
@@ -1,4 +1,5 @@
 import re
+from anytree import Node, RenderTree, PreOrderIter
 from enum import Enum, unique
 
 
@@ -103,13 +104,152 @@ first_of_non_terminals = {
     "Arg-list-prime": ", NUM",
 }
 
+parse_tree_file = open('parse_tree.txt', 'w')
+syntax_errors_file = open('syntax_errors.txt', 'w')
+
 
 class Parser(object):
-    def __init__(self, next_token):
-        self.token = next_token
+    def __init__(self, input_file):
+        if not os.path.isabs(input_file):
+            input_file = os.path.join(script_dir, input_file)
+        self.scanner = Scanner(input_file)
+        self.semantic_analyzer = SemanticAnalyser()
+        self.code_generator = CodeGen()
+        self._syntax_errors = []
+        self.root = Node("Program")  # Start symbol
+        self.parse_tree = self.root
+        self.stack = [Node("$"), self.root]
 
-    def parser(self, token):
-        if token == "int":
+        self.parse_tree_file = os.path.join(script_dir, "output", "parse_tree.txt")
+        self.syntax_error_file = os.path.join(script_dir, "errors", "syntax_errors.txt")
+
+    @property
+    def syntax_errors(self):
+        syntax_errors = []
+        if self._syntax_errors:
+            for lineno, error in self._syntax_errors:
+                syntax_errors.append(f"#{lineno} : Syntax Error! {error}\n")
+        else:
+            syntax_errors.append("There is no syntax error.\n")
+        return "".join(syntax_errors)
+
+    def save_parse_tree(self):
+        with open(self.parse_tree_file, "w", encoding="utf-8") as f:
+            for pre, _, node in RenderTree(self.parse_tree):
+                if hasattr(node, "token"):
+                    f.write(f"{pre}{node.token}\n")
+                else:
+                    f.write(f"{pre}{node.name}\n")
+
+    def save_syntax_errors(self):
+        with open(self.syntax_error_file, "w") as f:
+            f.write(self.syntax_errors)
+
+    def _remove_node(self, node):
+        try:
+            # remove node from the parse tree
+            parent = list(node.iter_path_reverse())[1]
+            parent.children = [c for c in parent.children if c != node]
+        except IndexError:
             pass
-        for i in first_of_non_terminals.keys():
-            pass
+
+    def _clean_up_tree(self):
+        ''' remove non terminals and unmet terminals from leaf nodes '''
+        remove_nodes = []
+        for node in PreOrderIter(self.parse_tree):
+            if not node.children and not hasattr(node, "token") and node.name != "EPSILON":
+                remove_nodes.append(node)
+
+        for node in remove_nodes:
+            self._remove_node(node)
+
+    def parse(self):
+        clean_up_needed = False
+        token = self.scanner.get_next_token()
+        new_nodes = []
+        while True:
+            token_type, a = token
+            if token_type in ("ID", "NUM"):  # parser won't understand the lexim input in this case
+                a = token_type
+
+            current_node = self.stack[-1]  # check the top of the stack
+            X = current_node.name
+
+            if X.startswith("#SA"):  # X is an action symbol for semantic analyzer
+                if X == "#SA_DEC_SCOPE" and a == "ID":
+                    curr_lexim = self.scanner.id_to_lexim(token[1])
+                self.semantic_analyzer.semantic_check(X, token, self.scanner.line_number)
+                self.stack.pop()
+                if X == "#SA_DEC_SCOPE" and a == "ID":
+                    token = (token[0], self.scanner.update_symbol_table(curr_lexim))
+            elif X.startswith("#CG"):  # X is an action symbol for code generator
+                self.code_generator.code_gen(X, token)
+                self.stack.pop()
+            elif X in terminal_to_col.keys():  # X is a terminal
+                if X == a:
+                    if X == "$":
+                        break
+                    self.stack[-1].token = self.scanner.token_to_str(token)
+                    self.stack.pop()
+                    token = self.scanner.get_next_token()
+                else:
+                    SymbolTableManager.error_flag = True
+                    if X == "$":  # parse stack unexpectedly exhausted
+                        # self._clean_up_tree()
+                        break
+                    self._syntax_errors.append((self.scanner.line_number, f'Missing "{X}"'))
+                    self.stack.pop()
+                    clean_up_needed = True
+            else:  # X is non-terminal
+                # look up parsing table which production to use
+                col = terminal_to_col[a]
+                row = non_terminal_to_row[X]
+                prod_idx = parsing_table[row][col]
+                rhs = productions[prod_idx]
+
+                if "SYNCH" in rhs:
+                    SymbolTableManager.error_flag = True
+                    if a == "$":
+                        self._syntax_errors.append((self.scanner.line_number, "Unexpected EndOfFile"))
+                        # self._clean_up_tree()
+                        clean_up_needed = True
+                        break
+                    missing_construct = non_terminal_to_missing_construct[X]
+                    self._syntax_errors.append((self.scanner.line_number, f'Missing "{missing_construct}"'))
+                    self._remove_node(current_node)
+                    self.stack.pop()
+                elif "EMPTY" in rhs:
+                    SymbolTableManager.error_flag = True
+                    self._syntax_errors.append((self.scanner.line_number, f'Illegal "{a}"'))
+                    token = self.scanner.get_next_token()
+                else:
+                    self.stack.pop()
+                    for symbol in rhs:
+                        if not symbol.startswith("#"):
+                            new_nodes.append(Node(symbol, parent=current_node))
+                        else:
+                            new_nodes.append(Node(symbol))
+
+                    for node in reversed(new_nodes):
+                        if node.name != "EPSILON":
+                            self.stack.append(node)
+                # print(f"{X} -> {' '.join(rhs)}")  # prints out the productions used
+                new_nodes = []
+
+        self.semantic_analyzer.eof_check(self.scanner.line_number)
+        if clean_up_needed:
+            self._clean_up_tree()
+        self.code_generator.code_gen("FINISH_PROGRAM", None)
+
+
+if __name__ == "__main__":
+    input_path = os.path.join(script_dir, "input/input_simple.c")
+    parser = Parser(input_path)
+    parser.parse()
+    parser.save_parse_tree()
+    parser.save_syntax_errors()
+    parser.scanner.save_lexical_errors()
+    parser.scanner.save_symbol_table()
+    parser.scanner.save_tokens()
+    parser.semantic_analyzer.save_semantic_errors()
+    parser.code_generator.save_output()
